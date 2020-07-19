8b1798c342e56f0665da0f23c1b753473525ac88
Cameron Graham <cameron.g882@gmail.com>
2020-07-18 21:40:33 -0400

    cleared body, decided to make the rows dynamically

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+console.log(moment().hour());
+// console.log($("#that").data("time"));
+// var hourEl = $(".row").data("time");
+// var currentHour = moment().hour();
+
+// function startPage() {
+
+//     $(".row").each(function() {
+//         if (currentHour == hourEl) {
+//             $(this).addClass("present");
+//         } if (currentHour > hourEl) {
+//             $(this).addClass("past");
+//         } else {
+//             $(this).addClass("future");
+//         }
+//     })
+// }
+
+var bridgesName = ["Bridgette", "Caroline", "Olivares","is in love with", "Cameron"]
+
+function herName(){
+    for (let index = 0; index < bridgesName.length; index++) {
+        alert(bridgesName[index]);
+        
+    }
+}
+herName();
+
+// document.addEventListener("click", function(){
+
+//     console.log($(this).getAttr.data.time)
+// })
