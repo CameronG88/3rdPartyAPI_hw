e3e05c7ac0f3b196b5571d3b8a7d453cfd9e71f9
Cameron Graham <cameron.g882@gmail.com>
2020-08-02 23:19:26 -0400

    added divs to the html with ids to pull on when setting/getting notes from local storage. Added local storage functionalty to save buttons

--- Assets/script.js
+++ Assets/script.js
@@ -3,28 +3,20 @@ console.log(currentHour);
 var containerDiv = $("#hourContainer");
 startPage();
 function startPage() {
-    var hourDiv = $("<div>").addClass("timeblock row");
-    var newText = $("<a>");
-    for (let index = 9; index < 18; index++) {
-        var elHour = moment().hour(index).format("H");
-        console.log(elHour);
+  
+    for (let index = 8; index < 19; index++) {
+        $("#" + index).val(localStorage.getItem(index));
+        // console.log(localStorage.getitem(index));
+        // var elHour = moment().hour(index).format("H");
+        // console.log(elHour);
                 
-        // if (currentHour == elHour) {
-        //     newText.text(elHour);
-        //     hourDiv.prepend(newText);
-        //     $(hourDiv).addClass("present");
-        //     containerDiv.append(hourDiv);
-        // } if (currentHour > elHour) {
-        //     newText.text(elHour);
-        //     hourDiv.prepend(newText);
-        //     $(hourDiv).addClass("past");
-        //     containerDiv.append(hourDiv);
-        // } else {
-        //     newText.text(elHour);
-        //     hourDiv.prepend(newText);
-        //     $(hourDiv).addClass("future");
-        //     containerDiv.append(hourDiv);
-        // }
              
     }
 }
+
+// click function to save the note into local storage
+$(".saveBtn").on("click", function (event){
+    event.preventDefault();
+
+    localStorage.setItem($(this).data('id'), $("#" + $(this).data("id")).val())
+})
