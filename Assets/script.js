b6216649f62f2a1ae685d510069dbd2797e031b1
Cameron Graham <cameron.g882@gmail.com>
2020-08-02 22:55:51 -0400

    tried to dynamically create the divs for the time blocks but I tihnk in this case it's better to hard code the html

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,30 @@
-console.log(moment().hour());
-// console.log($("#that").data("time"));
-// var hourEl = $(".row").data("time");
-// var currentHour = moment().hour();
-
-// function startPage() {
-
-//     $(".row").each(function() {
-//         if (currentHour == hourEl) {
-//             $(this).addClass("present");
-//         } if (currentHour > hourEl) {
-//             $(this).addClass("past");
-//         } else {
-//             $(this).addClass("future");
-//         }
-//     })
-// }
-
-var bridgesName = ["Bridgette", "Caroline", "Olivares","is in love with", "Cameron"]
-
-function herName(){
-    for (let index = 0; index < bridgesName.length; index++) {
-        alert(bridgesName[index]);
-        
+var currentHour = moment().format("H");
+console.log(currentHour);
+var containerDiv = $("#hourContainer");
+startPage();
+function startPage() {
+    var hourDiv = $("<div>").addClass("timeblock row");
+    var newText = $("<a>");
+    for (let index = 9; index < 18; index++) {
+        var elHour = moment().hour(index).format("H");
+        console.log(elHour);
+                
+        // if (currentHour == elHour) {
+        //     newText.text(elHour);
+        //     hourDiv.prepend(newText);
+        //     $(hourDiv).addClass("present");
+        //     containerDiv.append(hourDiv);
+        // } if (currentHour > elHour) {
+        //     newText.text(elHour);
+        //     hourDiv.prepend(newText);
+        //     $(hourDiv).addClass("past");
+        //     containerDiv.append(hourDiv);
+        // } else {
+        //     newText.text(elHour);
+        //     hourDiv.prepend(newText);
+        //     $(hourDiv).addClass("future");
+        //     containerDiv.append(hourDiv);
+        // }
+             
     }
 }
-herName();
-
-// document.addEventListener("click", function(){
-
-//     console.log($(this).getAttr.data.time)
-// })
